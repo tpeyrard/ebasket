8644c61a8034882049216e460f7b46e24e063a65
Thomas Peyrard <thomas.peyrard@gmail.com>
2017-01-19 22:23:32 +0100

    Support Cart with 1 item bought among 1 possible article

--- src/cart.js
+++ src/cart.js
@@ -1,9 +1,20 @@
-function Cart(items) {
-	this._items = items;
+function Cart(articlesAndCarts) {
+	this.content = articlesAndCarts;
+}
+
+function isEmpty(jsonContent) {
+	return Object.keys(jsonContent).length === 0 && jsonContent.constructor === Object
 }
 
 Cart.prototype.checkout = function() {
-	return 0;
+	if (isEmpty(this.content)) {
+		return 0
+	}
+	
+	quantity = this.content.carts[0].items[0].quantity
+	price = this.content.articles[0].price
+
+	return quantity * price;
 };
 
 
--- tests/level1/level-1-test.js
+++ tests/level1/level-1-test.js
@@ -6,7 +6,25 @@ var Cart = require('./../../src/cart');
 describe('Level1', function() {
 
 	it('checkout() should return 0 when no items are ordered', function() {
-		var cart = new Cart([]);
+		var cart = new Cart({});
 		expect(cart.checkout()).to.equal(0);
 	});
+
+
+	it('checkout() should return the correct amount for one cart with one article', function() {
+		var cart = new Cart({
+			"articles": [
+			{ "id": 1, "name": "water", "price": 100 }
+			],
+			"carts": [
+			{
+				"id": 1,
+				"items": [
+				{ "article_id": 1, "quantity": 6 }
+				]
+			}
+			]
+		});
+		expect(cart.checkout()).to.equal(600);
+	});	
 });
